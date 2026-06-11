bfc11ca5faf2c60f747be2fe029a3aab906709c4
ጊጠሃጕቿዪዕ <58253908+zmykerd@users.noreply.github.com>
2026-06-11 15:59:22 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,69 +1,41 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
-
 android {
     namespace = "com.mykerd.panic"
     compileSdk = 36
-
     defaultConfig {
         applicationId = "com.mykerd.panic"
         minSdk = 23
         targetSdk = 36
         versionCode = 1
-        versionName = "9.0"
-
+        versionName = "10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        resourceConfigurations += listOf("en", "it")
-
     }
-
     buildTypes {
         release {
             isMinifyEnabled = true
-
             isShrinkResources = true
-
             isCrunchPngs = true
-
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
     }
-
     buildFeatures {
-        compose = true
+        compose = false
     }
 }
-
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.material3)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    //implementation(libs.androidx.lifecycle.runtime.ktx)
 }
